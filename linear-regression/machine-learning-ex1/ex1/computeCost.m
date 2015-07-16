24784f4fc2a0603b6c07c41a8bcd9c8dcb07ebfb
aagrawal10 <akashgolpuria10@gmail.com>
2015-07-17 01:56:27 +0530

    Removing class related comments

--- linear-regression/machine-learning-ex1/ex1/computeCost.m
+++ linear-regression/machine-learning-ex1/ex1/computeCost.m
@@ -6,16 +6,11 @@ function J = computeCost(X, y, theta)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta
-%               You should set J to the cost.
+% Main vector logic to compute J
 
 C = X * theta - y;
 J = sum(C .^ 2) / (2 * m);
 
-% =========================================================================
+% The value of J will be returned from this function
 
 end
