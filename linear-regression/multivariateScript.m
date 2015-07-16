93b0671ec95c015f115019c91a493072a105ee3f
aagrawal10 <akashgolpuria10@gmail.com>
2015-07-17 02:46:25 +0530

    Modifying alpha values and fixing TODOs

--- linear-regression/multivariateScript.m
+++ linear-regression/multivariateScript.m
@@ -36,8 +36,8 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
-num_iters = 400;
+alpha = 1.0;
+num_iters = 20;
 
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
@@ -54,8 +54,9 @@ fprintf('Theta computed from gradient descent: \n');
 fprintf(' %f \n', theta);
 fprintf('\n');
 
-% TODO: Estimate the price of a 1650 sq-ft, 3 br house
-
+% Estimate the price of a 1650 sq-ft, 3 br house
+price = computeCost(X,y,theta);
+fprintf(' Estimated price %f \n', price);
 fprintf('Program paused. Press enter to continue.\n');
 pause;
 
@@ -81,8 +82,9 @@ fprintf(' %f \n', theta);
 fprintf('\n');
 
 
-% TODO: Estimate the price of a 1650 sq-ft, 3 br house
-
+% Estimate the price of a 1650 sq-ft, 3 br house
+price = computeCost(X,y,theta);
+fprintf(' Estimated price %f \n', price);
 
 % ============================================================
 
